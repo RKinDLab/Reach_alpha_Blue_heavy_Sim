b8ad20bcdc133421f1e20dafa9bfa1251b09987a
edward morgan <edmorgangh@gmail.com>
2024-02-24 17:21:18 -0600

    forward dynamics so loader added

---
+++ hardware/dynamics.cpp
@@ -0,0 +1,26 @@
+#include "ros2_control_blue_reach_5/dynamics.hpp"
+
+void casadi_reach_alpha_5::Dynamics::usage_cplusplus_checks(const std::string &name, const std::string &bin_name)
+{
+    std::cout << "---" << std::endl;
+    std::cout << "Usage from CasADi C++:" << std::endl;
+    std::cout << std::endl;
+
+    // Use CasADi's "external" to load a test compiled function
+    Function f = external(name, bin_name);
+
+    // Use like any other CasADi function
+    std::vector<double> x = {1, 2, 3, 4};
+    std::vector<DM> arg = {reshape(DM(x), 2, 2), 5};
+    std::vector<DM> res = f(arg);
+
+    std::cout << "result (0): " << res.at(0) << std::endl;
+    std::cout << "result (1): " << res.at(1) << std::endl;
+}
+
+bool casadi_reach_alpha_5::Dynamics::load_forward_dynamics(const std::string &name, const std::string &bin_name)
+{
+    // Use CasADi's "external" to load forward_dynamics compiled function
+    forward_dynamics = external(name, bin_name);
+    return true;
+}
